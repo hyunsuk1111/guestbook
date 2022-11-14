675eae8196dd8296462f7b7ea9aeee5eac941ee7
hyunsuk1111 <epsxkf@gmail.com>
2022-11-14 20:21:44 +0900

    상세 페이지 및 수정 버튼 생성

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -54,7 +55,7 @@ public class GuestbookController {
     }
 
     @GetMapping("/read")
-    public void read(Long gno, Model model){
+    public void read(Long gno, Model model,@ModelAttribute("requestDTO") PageRequestDTO requestDTO){
 
         log.info("gno : " + gno);
 
@@ -62,7 +63,7 @@ public class GuestbookController {
 
         log.info("data" + dto);
 
-        model.addAttribute("result", dto);
+        model.addAttribute("dto", dto);
     }
 
 }
