d07fe36597d914d5bf88f6dfcf6136547d85a7c9
hyunsuk1111 <epsxkf@gmail.com>
2022-11-20 20:12:53 +0900

    페이지 수정 기능 추가

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -60,6 +60,16 @@ public class GuestbookServiceImpl implements GuestbookService{
     @Override
     public void modify(GuestbookDTO dto) {
 
+        Optional<Guestbook> result = repository.findById(dto.getGno());
+
+        if(result.isPresent()){
+            Guestbook entity = result.get();
+
+            entity.changeTitle(dto.getTitle());
+            entity.changeContent(dto.getContent());
+
+            repository.save(entity);
+        }
     }
 
     @Override
